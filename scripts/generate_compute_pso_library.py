818523ffb6345ca4981d7d468860dc1681103093
Robert <33040161+rtryan98@users.noreply.github.com>
2024-04-17 09:10:39 +0200

    Add safety mechanisms to codegen, allow for permutation-less shaders

--- scripts/generate_compute_pso_library.py
+++ scripts/generate_compute_pso_library.py
@@ -84,15 +84,9 @@ $FUNCTIONS
 }
 """
 
-def generate_permutations(shader, source_path: str, binary_base_path: str) -> Tuple[str, str, str, str, str, str, str]:
-    permutation_index_lists = []
-    for permutation_group in shader["permutation_groups"]:
-        if permutation_group["type"] == "int":
-            permutation_index_lists.append(list(range(0, len(permutation_group["define_values"][0]))))
-        elif permutation_group["type"] == "bool":
-            permutation_index_lists.append([0, 1])
-    permutation_index_tuples = list(itertools.product(*permutation_index_lists))
-
+def generate_permutations(shader, source_path: str, binary_base_path: str) -> Tuple[str, str, str, str, str, str, str, bool]:
+    has_permutations = "permutation_groups" in shader and len(shader["permutation_groups"]) > 0
+    permutation_count = 1
     name_list = ""
     metadata = ""
     fn_args = ""
@@ -101,47 +95,58 @@ def generate_permutations(shader, source_path: str, binary_base_path: str) -> Tu
     first_element = ""
     permutation_sizes = ""
 
-    for permutation_group in shader["permutation_groups"]:
-        fn_arg_names += permutation_group['name'] + ", "
-        if permutation_group["type"] == "int":
-            fn_args += f"uint32_t {permutation_group['name']}, "
-            if "swizzle_define_values" in permutation_group and permutation_group["swizzle_define_values"]:
-                switch_cases = ""
-                for i, define_value in enumerate(permutation_group["define_values"][0]):
-                    switch_cases += select_shader_switch_case_template_string_cpp.replace("$CASE", str(define_value)).replace("$ARG", permutation_group["name"]).replace("$TARGET", str(i))
-                fn_switches += select_shader_switch_template_string_cpp.replace("$ARG", permutation_group["name"]).replace("$CASES", switch_cases)
-                permutation_sizes += f"{len(permutation_group['define_values'][0])}, "
-        elif permutation_group["type"] == "bool":
-            fn_args += f"bool {permutation_group['name']}, "
-            permutation_sizes += "2, "
+    if has_permutations:
+        permutation_index_lists = []
+        for permutation_group in shader["permutation_groups"]:
+            if permutation_group["type"] == "int":
+                permutation_index_lists.append(list(range(0, len(permutation_group["define_values"][0]))))
+            elif permutation_group["type"] == "bool":
+                permutation_index_lists.append([0, 1])
+        permutation_index_tuples = list(itertools.product(*permutation_index_lists))
+        permutation_count = len(permutation_index_tuples)
 
-    for permutation in permutation_index_tuples:
-        name = shader["name"]
-        defines = ""
-        for i, tuple_value in enumerate(permutation):
-            permutation_element = shader["permutation_groups"][i]
-            if permutation_element["type"] == "int":
-                name += "_" + (permutation_element["name"] if "use_name" in permutation_element and permutation_element["use_name"] else "") + str(permutation_element["define_values"][0][tuple_value])
-                for j, define_name in enumerate(permutation_element["define_names"]):
-                    defines += f"\"{define_name}={permutation_element['define_values'][j][tuple_value]}\", "
-            elif permutation_element["type"] == "bool":
-                if tuple_value == 1:
-                    name += "_" + permutation_element["name"]
-                    defines += f"\"{permutation_element['define_name']}=1\", "
-                else:
-                    defines += f"\"{permutation_element['define_name']}=0\", "
-        binary_path = binary_base_path + name + ".bin"
-        if first_element == "":
-            first_element = name
+        for permutation_group in shader["permutation_groups"]:
+            fn_arg_names += permutation_group['name'] + ", "
+            if permutation_group["type"] == "int":
+                fn_args += f"uint32_t {permutation_group['name']}, "
+                if "swizzle_define_values" in permutation_group and permutation_group["swizzle_define_values"]:
+                    switch_cases = ""
+                    for i, define_value in enumerate(permutation_group["define_values"][0]):
+                        switch_cases += select_shader_switch_case_template_string_cpp.replace("$CASE", str(define_value)).replace("$ARG", permutation_group["name"]).replace("$TARGET", str(i))
+                    fn_switches += select_shader_switch_template_string_cpp.replace("$ARG", permutation_group["name"]).replace("$CASES", switch_cases)
+                    permutation_sizes += f"{len(permutation_group['define_values'][0])}, "
+            elif permutation_group["type"] == "bool":
+                fn_args += f"bool {permutation_group['name']}, "
+                permutation_sizes += "2, "
 
-        name_list += "\n    " + name + ","
-        metadata += compute_pipeline_template_string.replace(
-            "$NAME", "\"" + name + "\"").replace(
-            "$BINARY_PATH", "\"" + binary_path + "\"").replace(
-            "$SOURCE_PATH", "\"" + source_path + "\"").replace(
-            "$DEFINES", defines)
-    
-    return name_list, metadata, shader["name"], fn_args[:-2], fn_switches, first_element, permutation_sizes, fn_arg_names
+        for permutation in permutation_index_tuples:
+            name = shader["name"]
+            defines = ""
+            for i, tuple_value in enumerate(permutation):
+                permutation_element = shader["permutation_groups"][i]
+                if permutation_element["type"] == "int":
+                    name += "_" + (permutation_element["name"] if "use_name" in permutation_element and permutation_element["use_name"] else "") + str(permutation_element["define_values"][0][tuple_value])
+                    for j, define_name in enumerate(permutation_element["define_names"]):
+                        defines += f"\"{define_name}={permutation_element['define_values'][j][tuple_value]}\", "
+                elif permutation_element["type"] == "bool":
+                    if tuple_value == 1:
+                        name += "_" + permutation_element["name"]
+                        defines += f"\"{permutation_element['define_name']}=1\", "
+                    else:
+                        defines += f"\"{permutation_element['define_name']}=0\", "
+            binary_path = binary_base_path + name + ".bin"
+            if first_element == "":
+                first_element = name
+
+            name_list += "\n    " + name + ","
+            metadata += compute_pipeline_template_string.replace(
+                "$NAME", "\"" + name + "\"").replace(
+                "$BINARY_PATH", "\"" + binary_path + "\"").replace(
+                "$SOURCE_PATH", "\"" + source_path + "\"").replace(
+                "$DEFINES", defines)
+
+    has_permutations = has_permutations and permutation_count > 1
+    return name_list, metadata, shader["name"], fn_args[:-2], fn_switches, first_element, permutation_sizes, fn_arg_names, has_permutations
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -160,19 +165,20 @@ if __name__ == '__main__':
     for shader_json_file in args.shader_json_files:
         json_object = json.load(open(shader_json_file))
         shader_source_path = shader_json_file.replace(args.source_base_path, "").replace(".json", ".hlsl")
-        pipeline_string, metadata, name, fn_args, fn_switches, first_element, permutation_sizes, fn_arg_names = generate_permutations(json_object, "." + shader_source_path, args.binary_base_path + shader_source_path.replace(".hlsl", ""))
+        pipeline_string, metadata, name, fn_args, fn_switches, first_element, permutation_sizes, fn_arg_names, has_permutations = generate_permutations(json_object, "." + shader_source_path, args.binary_base_path + shader_source_path.replace(".hlsl", ""))
         compute_pipelines_string += pipeline_string
         compute_pipeline_metadata_string += metadata
-        compute_pipeline_select_shader_string += select_shader_template_string.replace(
-            "$NAME", name).replace(
-            "$ARGS", fn_args)
-        compute_pipeline_select_shader_cpp_string += select_shader_template_string_cpp.replace(
-            "$NAME", name).replace(
-            "$ARGS", fn_args).replace(
-            "$SWITCHES", fn_switches).replace(
-            "$FIRST_ENUM_ELEMENT", "Compute_Pipelines::" + first_element).replace(
-            "$PERMUTATION_SIZES", permutation_sizes).replace(
-            "$ARG_NAMES", fn_arg_names)
+        if has_permutations:
+            compute_pipeline_select_shader_string += select_shader_template_string.replace(
+                "$NAME", name).replace(
+                "$ARGS", fn_args)
+            compute_pipeline_select_shader_cpp_string += select_shader_template_string_cpp.replace(
+                "$NAME", name).replace(
+                "$ARGS", fn_args).replace(
+                "$SWITCHES", fn_switches).replace(
+                "$FIRST_ENUM_ELEMENT", "Compute_Pipelines::" + first_element).replace(
+                "$PERMUTATION_SIZES", permutation_sizes).replace(
+                "$ARG_NAMES", fn_arg_names)
 
     output_string_hpp = output_template_string_hpp.replace(
         "$COMPUTE_PIPELINES", compute_pipelines_string).replace(
