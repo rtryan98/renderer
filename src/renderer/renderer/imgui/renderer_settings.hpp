a5bb3fb1507a7f1d4b9e456f94fc7da8d21ef63b
Robert <33040161+rtryan98@users.noreply.github.com>
2024-05-11 17:11:56 +0200

    add render settings

---
+++ src/renderer/renderer/imgui/renderer_settings.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace ren
+{
+class Settings_Base
+{
+public:
+    virtual ~Settings_Base() noexcept = default;
+
+    const [[nodiscard]] std::string& get_name() noexcept;
+    virtual void process_gui() = 0;
+
+protected:
+    Settings_Base(const std::string_view name) noexcept;
+
+private:
+    std::string m_name;
+};
+
+class Renderer_Settings
+{
+public:
+    void process_gui(bool* active);
+    void add_settings(Settings_Base* settings) noexcept;
+
+private:
+    float m_listbox_width = 150.f;
+    uint32_t m_selected = 0;
+    std::vector<Settings_Base*> m_settings = {};
+};
+}
