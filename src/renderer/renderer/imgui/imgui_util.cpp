4caa3874c4c0655dee7b5bfb38ccc551954e3f6c
Robert <33040161+rtryan98@users.noreply.github.com>
2024-05-12 21:28:34 +0200

    created imgui utilities

---
+++ src/renderer/renderer/imgui/imgui_util.cpp
@@ -0,0 +1,19 @@
+#include "renderer/imgui/imgui_util.hpp"
+
+#include <imgui.h>
+
+namespace ren
+{
+void imgui_help_marker(const char* text, bool is_same_line)
+{
+    if (is_same_line) ImGui::SameLine();
+    ImGui::TextDisabled("(?)");
+    if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort) && ImGui::BeginTooltip())
+    {
+        ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+        ImGui::TextUnformatted(text);
+        ImGui::PopTextWrapPos();
+        ImGui::EndTooltip();
+    }
+}
+}
---
+++ src/renderer/renderer/imgui/imgui_util.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace ren
+{
+void imgui_help_marker(const char* text, bool is_same_line = true);
+}
